290a50c1092abbd296ab4f1df7bb75fdf55290c2
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2023-02-19 23:33:17 +0100

    progress

--- app/src/components.js
+++ app/src/components.js
@@ -16,23 +16,70 @@ export function Product(props) {
   )
 }
 
+const subCatogeries = {
+  elecontrins: ["CPU", "GPU", "Herní konzole"],
+  sport: ["Zimní", "Fitness", "Výživa"],
+  house: ["Mopy", "Pánve", "Prášky na praní"],
+  garden: ["Sekačky", "Skleníky", "Brusky"]
+}
+var selection = 0
+
 export function Header() {
+  const menu = document.getElementById("dropdown-menu")
+  function hideDropDownMenu() {
+    menu.style.visibility = "collapse"
+  }
+  function showDropDownMenu() {
+    menu.style.visibility = "visible"
+  }
+  function dropDownMenu(category) {
+    if(category === "electronics") {
+      console.log("electronics")
+      selection = 0
+    }
+    if(category === "sport") {
+      console.log("sport")
+      selection = 1
+    }
+    if(category === "house") {
+      console.log("house")
+      selection = 2
+    }
+    if(category === "garden") {
+      console.log("garden")
+      selection = 3
+    }
+    showDropDownMenu()
+  }
   return (
     <header>
-      <ul>
-        <li>
-          <a href="#notebooks">Notebooky</a>
-        </li>
-        <li>
-          <a href="#gamingconsoles">Herní konzole</a>
-        </li>
-        <li>
-          <a href="#monitors">Monitory</a>
-        </li>
-        <li>
-          <a href="#gpus">Grafické karty</a>
-        </li>
-      </ul>
+      <div className="logo-name">
+        <h1>logo</h1>
+        <h1>name</h1>
+      </div>
+      <div className="categories">
+        <ul>
+          <li>
+            <a onClick={() => dropDownMenu("electronics")}>Elektronika</a>
+          </li>
+          <li>
+            <a onClick={() => dropDownMenu("sport")}>Sport</a>
+          </li>
+          <li>
+            <a onClick={() => dropDownMenu("house")}>Dům</a>
+          </li>
+          <li>
+            <a onClick={() => dropDownMenu("garden")}>Zahrada</a>
+          </li>
+        </ul>
+      </div>
+      <div id="dropdown-menu">
+        <div className="dropdown-menu-background" onClick={() => hideDropDownMenu()}></div>
+        <div className="dropdown-menu-active">
+          {selection == 1 ? <p>bruh</p> : <p>nebruh</p> 
+          }
+        </div>  
+      </div>
     </header>
   );
 }
