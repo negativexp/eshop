057c9e6d6cdd467a989248b747e3895e8b986f93
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2023-03-03 22:24:37 +0100

    fixed not showing single product bug

--- app/src/components/SingleProduct.js
+++ app/src/components/SingleProduct.js
@@ -1,46 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { useFetcher, useParams } from "react-router-dom";
-import { searchForItems } from "./searchForItems";
 
 export function SingleProduct({ addToCart }) {
   const { productID } = useParams();
-  const [product, setProduct] = useState([]);
+  const [product, setProduct] = useState();
 
   useEffect(() => {
     fetch("/api/products")
       .then((res) => res.json())
       .then((data) => {
-        const matchingObjects = [];
-        //everything has to be lowercase to check if the search value matches
-        //the value we are trying to look for
-        //iritate through every object in apiproducts
-        for (let i = 0; i < data.length; i++) {
-          //get object
-          const obj = data[i];
-          for (let prop in obj) {
-            //chceck if field is an array in object
-            if (Array.isArray(obj[prop])) {
-              //check if it includes the value
-              if (obj[prop].toLowerCase().includes(productID.toLowerCase())) {
-                //if so, push the object into matching objects
-                matchingObjects.push(obj);
-                break;
-              }
-            } else {
-              if (obj[prop].toLowerCase().includes(productID.toLowerCase())) {
-                matchingObjects.push(obj);
-                break;
-              }
-            }
-          }
-        }
-        setProduct(matchingObjects);
+
+        const singleProduct = data.find((product) => product._id === productID);
+        setProduct(singleProduct)
+      
       });
   }, [0]);
 
   return (
     <div className="singleProduct">
-      {product.length === 0 ? (
+      {product == undefined ? (
         <div className="loading">
           <h1>loading...</h1>
         </div>
@@ -52,14 +30,14 @@ export function SingleProduct({ addToCart }) {
               <img
                 src={
                   "http://localhost:5000/images/products/" +
-                  product[0].productID +
+                  product.productID +
                   ".jpg"
                 }
               />
             </div>
             <div className="productInfo">
-              <h1>{product[0].title}</h1>
-              <p>{product[0].longDescription}</p>
+              <h1>{product.title}</h1>
+              <p>{product.longDescription}</p>
             </div>
           </div>
           <div className="addtocart">
