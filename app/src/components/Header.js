999e44d9eb4ef9b23c12b94ad8d1cf12ff225a8e
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2023-03-11 09:26:55 +0100

    trying to optimize search helper

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -2,8 +2,7 @@ import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { searchForItems } from "./searchForItems";
 
-export function Header({cartPrice, products, categories}) {
-
+export function Header({ cartPrice, products, categories }) {
   const [searchQuery, setSearchQuery] = useState("");
   const navigate = useNavigate();
 
@@ -13,18 +12,22 @@ export function Header({cartPrice, products, categories}) {
     }
   };
 
-  const [searchHelper, setSearchHelper] = useState([])
+  const [searchHelper, setSearchHelper] = useState([]);
   const handleInputChange = (event) => {
     setSearchQuery(event.target.value);
-    if(event.target.value != "") {
-      const productsFilter = searchForItems(event.target.value, products)
-      const categoriesFilter = categories.filter(item => {
-        return item.category.toLowerCase().includes(searchQuery.toLowerCase()) || 
-      item.subCategories.some(sub => sub.toLowerCase().includes(searchQuery.toLowerCase()));
-      })
-      setSearchHelper([productsFilter, categoriesFilter])
+    if (event.target.value != "") {
+      const productsFilter = searchForItems(event.target.value, products);
+      const categoriesFilter = categories.filter((item) => {
+        return (
+          item.category.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          item.subCategories.some((sub) =>
+            sub.toLowerCase().includes(searchQuery.toLowerCase())
+          )
+        );
+      });
+      setSearchHelper([productsFilter, categoriesFilter]);
     } else {
-      setSearchHelper([])
+      setSearchHelper([]);
     }
   };
 
@@ -36,22 +39,24 @@ export function Header({cartPrice, products, categories}) {
       </div>
       <div className="buttons">
         <div className="search">
-          <input id="searchbar" type="text" onChange={handleInputChange} />
+          <div>
+            <input id="searchbar" type="text" onChange={handleInputChange} />
+            {searchHelper.length === 0 ? (
+              <div className="searchHelper"></div>
+            ) : (
+              <div className="searchHelper">
+                {searchHelper[0].map((item) => (
+                  <p>{item.title}</p>
+                ))}
+                {searchHelper[1].map((item) => (
+                  <p>{item.category}</p>
+                ))}
+              </div>
+            )}
+          </div>
           <button id="searchbutton" onClick={handleSearchQuery}>
             Hledat
           </button>
-          {searchHelper.length === 0 ? (
-            <p></p>
-          ) : (
-            <div className="searchHelper">
-            {searchHelper[0].map((item) => (
-              <p>{item.title}</p>
-            ))}
-                        {searchHelper[1].map((item) => (
-              <p>{item.category}</p>
-            ))}
-            </div >
-          )}
         </div>
         <Link to="/checkout">Košík ({cartPrice} Kč)</Link>
       </div>
